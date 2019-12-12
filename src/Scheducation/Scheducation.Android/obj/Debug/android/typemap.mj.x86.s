d971cb472d2a1a824ce594c4f5e36eb93ea6cd25
Jasoninirio <jasonini@bu.edu>
2019-12-12 17:13:18 -0500

    Added Scheducation App
    Deleted separate iOS and Android files and conjoined them into using C++ and C# libraries instead of Objective-C and Java, respectively.

---
+++ src/Scheducation/Scheducation.Android/obj/Debug/android/typemap.mj.x86.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: 09357422f965a19254b7d40695ecf255c07a5b9e */
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1420
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	145
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 372041
+	.include	"typemap.mj.inc"
