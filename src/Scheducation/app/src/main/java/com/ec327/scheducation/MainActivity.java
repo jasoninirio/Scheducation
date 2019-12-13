f939f9868a586c6df2b62721d1ef5ddc19bad608
jasoninirio <44911131+jasoninirio@users.noreply.github.com>
2019-12-13 15:55:54 -0500

    Created SQLite DB
    - Added a SQLite3 DB to MainActivity
    - Added two classes (TaskDbHelper and TaskContract)
    - TaskContract is the DB constant initializer
    - TaskDbHelper is the DB Helper

--- src/Scheducation/app/src/main/java/com/ec327/scheducation/MainActivity.java
+++ src/Scheducation/app/src/main/java/com/ec327/scheducation/MainActivity.java
@@ -3,22 +3,49 @@ package com.ec327.scheducation;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.DialogInterface;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.ListView;
+
+import com.ec327.scheducation.db.TaskContract;
+import com.ec327.scheducation.db.TaskDbHelper;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "MainActivity";
+    private TaskDbHelper mHelper;
+    private ListView mTaskListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mHelper = new TaskDbHelper(this);
+        mTaskListView = (ListView) findViewById(R.id.list_tasks);
+
         // FIX THIS TO SHOW activity_menu INSTEAD
         setContentView(R.layout.activity_task); // Sets Initial View
+
+        // Initializing Database
+        SQLiteDatabase db = mHelper.getReadableDatabase();
+        Cursor cursor = db.query(TaskContract.TaskEntry.TABLE,
+                new String[]{TaskContract.TaskEntry._ID, TaskContract.TaskEntry.COL_TABLE_TITLE},
+                null, null, null, null, null);
+        while (cursor.moveToNext()) {
+            int idx = cursor.getColumnIndex(TaskContract.TaskEntry.COL_TABLE_TITLE);
+            Log.d(TAG, "Task: " + cursor.getString(idx));
+        }
+        cursor.close();
+        db.close();
     }
 
     @Override
@@ -34,13 +61,22 @@ public class MainActivity extends AppCompatActivity {
                 final EditText taskEditText = new EditText(this);
                 AlertDialog dialog = new AlertDialog.Builder(this)
                         .setTitle("Add a New Task")
-                        .setMessage("What do you want to do next?") // Set Label System Here
+                        // Set Label System Here
+                        .setMessage("What do you want to do next?")
                         .setView(taskEditText)
                         .setPositiveButton("Add", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
                                 String task = String.valueOf(taskEditText.getText());
-                                Log.d(TAG, "Task to add: " + task);
+                                //Log.d(TAG, "Task to add: " + task); // EDITED OUT FOR DB
+                                SQLiteDatabase db = mHelper.getWritableDatabase();
+                                ContentValues values = new ContentValues();
+                                values.put(TaskContract.TaskEntry.COL_TABLE_TITLE, task);
+                                db.insertWithOnConflict(TaskContract.TaskEntry.TABLE,
+                                        null,
+                                        values,
+                                        SQLiteDatabase.CONFLICT_REPLACE);
+                                db.close();
                             }
                         })
                         .setNegativeButton("Cancel", null)
